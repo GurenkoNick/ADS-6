2bfa2e7866eec06d882d355f2eedfbd4f5245e1f
GurenkoNick <78843007+GurenkoNick@users.noreply.github.com>
2021-05-25 16:25:50 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -4,84 +4,89 @@
 
 template <typename T>
 class BST {
- public:
-  struct Node {
-    T data;
-    int count;
-    Node* left;
-    Node* right;
-  }
-  BST() :root(nullptr) {}
-  ~BST() {};
- 
-  void add(T data) {
-    root = addNode(root, data);
-  }
- 
-  void clear() {
-   if (root) {
-    delTree (root);
-    root = nullptr;
-   }
-  }
- 
-  int depth() {
-    return depthTree(root);
-  }
-  int search(T data) {
-    return countNodes(root, data);
-  }
- private:
-  Node* root;
-  Node* addNode(Node* root, T data) {
-    if (root == nullptr) {
-      root = new Node;
-      root->data = data;
-      root->count = 1;
-      root->left = root->right = nullptr;
-    } else if (root->data > data) {
-      root->left = addNode(root->left, data);
-    } else if (root->data < value) {
-      root->right = addNode(root->right, data);
-    } else {
-      root->count++;
-    }
-    return root;
-  }
- 
-  void delTree(Node* root) {
-   if (root == nullptr )
-    return 0;
-   else {
-    delTree(root->left);
-    delTree(root->right);
-    delete root;
-   }
-  }
+public:
+    struct Node {
+        T data;
+        int count;
+        Node* left;
+        Node* right;
+    };
+    BST() :root(nullptr) {}
+    ~BST() {};
 
-  int depthTree(Node* root) {
-    if (root == nullptr)
-      return 0;
-    if (root != nullptr) {
-      int Lh = depthTree(root->left);
-      int Rh = depthTree(root->right);
-      if (Lh > Rh)
-        return Lh + 1;
-      else 
-        return Rh + 1;
+    void add(T data) {
+        root = addNode(root, data);
     }
-  }
- 
-  int countNodes(Node* root, T data) {
-    if (root == nullptr)
-      return 0;
-    else {
-      if (root->data > data)
-        return countNodes(root->left, data);
-      if (root->data < data)
-        return countNodes(root->right, data);
-      return root->count;
+    void clear() {
+        if (root)
+        {
+            delTree(root);
+            root = nullptr;
+        }
     }
-  }
+    int depth() {
+        return depthTree(root) - 1;
+    }
+    int search(T data) {
+        return countNodes(root, data);
+    }
+private:
+    Node* root;
+    Node* addNode(Node* root, T data) {
+        if (root == nullptr) {
+            root = new Node;
+            root->data = data;
+            root->count = 1;
+            root->left = root->right = nullptr;
+        }
+        else if (root->data > data) {
+            root->left = addNode(root->left, data);
+        }
+        else if (root->data < data) {
+            root->right = addNode(root->right, data);
+        }
+        else {
+            root->count++;
+        }
+        return root;
+    }
+
+    void delTree(Node* root) {
+        if (root == nullptr)
+            return 0;
+        else {
+            delTree(root->left);
+            delTree(root->right);
+            delete root;
+        }
+    }
+
+    int depthTree(Node* root) {
+        if (root == nullptr)
+            return 0;
+            int Lh = depthTree(root->left);
+            int Rh = depthTree(root->right);
+            if (Lh > Rh)
+                return Lh + 1;
+            else
+                return Rh + 1;
+    }
+
+    int countNodes(Node* root, T data) {
+        if (root == nullptr)
+            return 0;
+        else {
+            if (root->data > data)
+                return countNodes(root->left, data);
+            if (root->data < data)
+                return countNodes(root->right, data);
+            return root->count;
+        }
+    }
 };
-#endif  // INCLUDE_BST_H_
+
+
+
+
+
+#endif
